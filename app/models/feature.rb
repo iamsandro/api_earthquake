60d22117ad4a77d40deda5357f9293ad8eb7f448
rubensant <ruben.santillan0902@gmail.com>
2024-04-07 11:14:11 -0500

    Create Feature Model

---
+++ app/models/feature.rb
@@ -0,0 +1,24 @@
+class Feature < ApplicationRecord
+    attribute :type, :string
+    attribute :external_id, :string
+    attribute :magnitude, :decimal
+    attribute :longitude, :decimal
+    attribute :latitude, :decimal
+    attribute :title, :string
+    attribute :tsunami, :boolean
+    attribute :time, :string
+    attribute :place, :string
+    attribute :external_url, :string 
+
+    # Enumerado para magType
+    enum magType: { md: 0, ml: 1, ms: 2, mw: 3, me: 4, mi: 5, mb: 6, mlg: 7 }
+
+    validates :title, presence: true
+    validates :place, presence: true
+    validates :mag_type, presence: true
+    validates :magnitude, presence: true
+    validates :longitude, presence: true
+    validates :latitude, presence: true
+    validates :external_url, presence: true
+
+end
