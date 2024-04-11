4c0c6d2c1e404fdae8aa2c6c8348d7ca31fbd574
rubensant <ruben.santillan0902@gmail.com>
2024-04-10 20:12:40 -0500

    Add handle errors to comments creation

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::CommentsController < ApplicationController
+
   def create
 
+   render json: {error: 'feature_id is mandatory'}, status: :bad_request unless params[:feature_id]&.present?
+   render json: {error: 'The comment is empty, body is mandatory'}, status: :bad_request unless params[:body]&.present?
+     
     data = {
       feature_id: params[:feature_id],
       body: params[:body]
@@ -8,17 +12,16 @@ class Api::V1::CommentsController < ApplicationController
 
     begin
       @new_coment = Comment.create!(data)
+      render json: @new_coment, status: 200
 
-      render json: @new_coment
-      return
     rescue ActiveRecord::RecordInvalid => e
-      puts "Error: #{e}"
+      render json: { error: e }, status: 404
+
     rescue ActiveRecord::RecordNotSaved => e
-      puts "Error de Conexión: #{e}"
-    ensure
-      puts "Fin"
+      render json: { error: e}, status: 503
+
     end
 
-
   end
+  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,16 @@ class Comment < ApplicationRecord
   attribute :feature_id, :string
 
   validates :body, presence: true
-  validates :feature_id, presence: true
+  validates :feature_id, presence: {message: "the feature_id is required"} 
+  validate :existence
 
   belongs_to :feature
+
+  private 
+
+  def existence
+     errors.add(:feature_id, " #{feature_id} not found")  unless Feature.exists?(id: feature_id)
+
+  end
 
 end
